ebf66bc5bedfdc64fb50704fa6c1adb1a75f837e
Mitchell Mennelle <mpmennelle@gmail.com>
2024-03-12 08:39:22 -0500

    added format condensor and better readability in generate with proper comma.

--- condesor.py
+++ condesor.py
@@ -1,12 +1,12 @@
 input_file_path = 'converted_output_file.txt'
-output_file_path = 'condensed_adjective.txt'
+output_file_path = 'condensed_noun.txt'
 
 # Read the input file
 with open(input_file_path, 'r') as file:
-
+    # Read lines and strip unwanted characters
     words = [line.strip(" ,\n'[]") for line in file]
 
-# divide the words list into chunks of 10
+# Function to divide the words list into chunks of 10
 def chunked_list(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
@@ -14,15 +14,16 @@ def chunked_list(lst, n):
 # Open the output file in write mode
 with open(output_file_path, 'w') as file:
     file.write("[")
-    first_line = True
+    first_chunk = True
     for chunk in chunked_list(words, 10):
-        if not first_line:
-            file.write("\n")
-        # Join the current chunked words, separated by ', '
+        # For each chunk after the first one, start a new line with a comma
+        if not first_chunk:
+            file.write(",\n")
+        else:
+            first_chunk = False
+        # Join the current chunk's words, separated by ', '
         output_string = ", ".join("'" + word + "'" for word in chunk)
         file.write(output_string)
-        first_line = False
     file.write("]")
 
 print("Transformation complete.")
-
