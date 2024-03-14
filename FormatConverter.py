3a3eefc065096b274c21fd06136f160a6a10461f
Mitchell Mennelle <mpmennelle@gmail.com>
2024-03-14 08:02:06 -0500

    updated format.

--- FormatConverter.py
+++ FormatConverter.py
@@ -4,7 +4,7 @@ def convert_lines(input_file, output_file):
             # Splitting each line at spaces and taking the second element, which is the target
             parts = line.strip().split(' ')
             if len(parts) >= 0:  # Ensure there are at least two parts to avoid IndexError
-                verb = parts[1]  # The adjective is the second element
+                verb = parts[1]  # Choose the correct position of the element.
                 # Write the formatted string to the output file
                 outfile.write(", '{}' ".format(verb) + "\n")
 
